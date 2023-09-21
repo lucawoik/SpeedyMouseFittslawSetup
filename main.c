113cc9a3aa5d2df1ece0b46cf4299e3370b5a89c
Isabell <iska1@web.de>
2023-09-21 11:30:19 +0200

    TEST-SETUP

--- main.c
+++ main.c
@@ -41,34 +41,6 @@ void finish()
 {
     logClicks();
     logTrials();
-    // write log files
-
-    // print Trials
-    /*
-    for(int i = 0; i < NUM_ITERATIONS; i++)
-    {
-        printf("%d %ld | %d %d %d %d | %d %d %d %d | %f %d %d %d\n", trials[i].id, trials[i].timestamp, trials[i].w, trials[i].d, trials[i].v, trials[i].a, trials[i].x_target, trials[i].y_target, trials[i].x_cursor, trials[i].y_cursor, trials[i].time, trials[i].clicks, trials[i].travel_distance, trials[i].success);
-    }
-    */
-
-    // print clicks
-    //for(int i = 0; i < click_count_total; i++)
-    //{
-    //    printf("%d %ld | %d %d %d %d | %d %d %d %d | %d %d\n", 
-    //            clicks[i].id,
-    //            clicks[i].timestamp,
-    //            clicks[i].w,
-    //            clicks[i].d,
-    //            clicks[i].v,
-    //            clicks[i].a,
-    //            clicks[i].x_target,
-    //            clicks[i].y_target,
-    //            clicks[i].x_cursor,
-    //            clicks[i].y_cursor,
-    //            clicks[i].distance,
-    //            clicks[i].success);
-    //}
-
     SDL_Quit();
     exit(1);
 }
@@ -153,13 +125,6 @@ void handleInput()
                                              0,
                                              0,
                                              0};
-
-                    // fixed: was caused by the cursor being too far away from the base line
-                    //if(target.x < -2000)
-                    //{
-                    //    printf("problem here!\n");
-                    //    finish();
-                    //}
 
                     if(DEBUG > 1) printf("target created: x %f y %f r %d vX %f vY %f\n", target.x, target.y, target.r, target.vX, target.vY);
 
