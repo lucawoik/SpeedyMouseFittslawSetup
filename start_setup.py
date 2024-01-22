07d6a3b78ae058da9237089e774b9b3b7d8c6e51
lucawoik <lucaleon.woik@gmail.com>
2024-01-22 14:42:09 +0100

    Delete sudo exection from start_setup.py

--- start_setup.py
+++ start_setup.py
@@ -12,7 +12,7 @@ while(True):
     os.system('python3 questionnaires/demographic_survey.py ' + str(participant_id))
     
     for trial in range(num_latency_levels):
-        os.system('sudo ./fittslaw-app/SpeedyMouseFittslaw {} {} {} {}'.format(event_handler, participant_id, trial, 0))
+        os.system('./fittslaw-app/SpeedyMouseFittslaw {} {} {} {}'.format(event_handler, participant_id, trial, 0))
         os.system('python3 questionnaires/nasa-tlx.py {} {} {}'.format(participant_id, trial, 0))
 
     os.system('python3 notification.py')
