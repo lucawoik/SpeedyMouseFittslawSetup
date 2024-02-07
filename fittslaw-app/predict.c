ad2eea26b9a0f7e8b2a74be9ff4cc93d7f308c62
lucawoik <lucaleon.woik@gmail.com>
2024-02-07 14:01:15 +0100

    Add emit function

--- fittslaw-app/predict.c
+++ fittslaw-app/predict.c
@@ -10,6 +10,23 @@ long intervalStop = 0;
 float intervalX = 0.0f;
 float intervalY = 0.0f;
 
+// creates an input event for the specified device
+// source: https://www.kernel.org/doc/html/v4.12/input/uinput.html
+void emit(int fd, int type, int code, int val)
+{
+    struct input_event ie;
+
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+
+    ie.time.tv_sec = 0;
+    ie.time.tv_usec = 0;
+
+    write(fd, &ie, sizeof(ie));
+}
+
+
 void *manipulateMouseEvents(void *arg)
 {
     // open the event handler
