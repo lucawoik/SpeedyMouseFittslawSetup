81e721d95f3f6537c7ecc858b2779cc99117ed54
lucawoik <lucaleon.woik@gmail.com>
2024-02-12 13:58:06 +0100

    Set initial buffer values to normalized zeroes

--- fittslaw-app/predict.c
+++ fittslaw-app/predict.c
@@ -27,8 +27,8 @@ void initCircularBuffer(CircularBuffer *buffer)
     buffer->front = -1;
 
     ResampledEvent eventZero;
-    eventZero.x = 0.5f;
-    eventZero.y = 0.5f;
+    eventZero.x = 0.430556f;
+    eventZero.y = 0.474576f;
 
     // init buffer with all zeros to be able to call the ann right from the beginning
     for (int i = 0; i < BUFFER_LENGTH; i++)
